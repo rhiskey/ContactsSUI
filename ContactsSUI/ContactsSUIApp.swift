f19df73e68a0308c5a87bb534675ee4d73fb5db8
rhiskey <first3395@gmail.com>
2022-05-05 10:16:28 +0300

    Initial Commit

---
+++ ContactsSUI/ContactsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSUIApp.swift
+//  ContactsSUI
+//
+//  Created by Владимир Киселев on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
